af858c5a3d389d210ee51b3a5d1042ebd00570e3
G384632023 <238463@st.takushoku-u.ac.jp>
2025-05-22 13:36:14 +0900

    kazuate.jsを新規作成

--- kazuate.js
+++ kazuate.js
@@ -1,36 +1,31 @@
-// 答え
 let kotae = Math.floor(Math.random() * 10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
-// 入力回数（予想回数）
 let kaisu = 0;
 
-
-let b = document.querySelector('button#guessButton');
+let b = document.querySelector('button#kotae');
 b.addEventListener('click', hantei);
 
-// 判定処理
 function hantei() {
-  // 入力値を取得して数値に変換
-  let input = document.getElementById("userInput").value;
-  let yoso = Number(input);
-
-  // 回数を更新
+let yos = document.querySelector('input[name="kotae"]');
+    let yoso = Number(yos.value);
   kaisu += 1;
 
   // コンソールに出力（HTMLには表示しない）
-  console.log(kaisu + '回目の予想: ' + yoso);
-
-  if (kaisu >= 4) {
-    console.log('答えは ' + kotae + ' でした。すでにゲームは終わっています。');
-    } else if (yoso === kotae) {
-    console.log('正解です。おめでとう!');
-    kaisu = 4; // ゲーム終了
-    } else if (kaisu === 3) {
-    console.log('まちがい。残念でした答えは ' + kotae + ' です。');
-    } else if (yoso < kotae) {
-    console.log('まちがい。答えはもっと大きいですよ。');
-    } else {
-    console.log('まちがい。答えはもっと小さいですよ。');
-    }
+ 
+  let ans = document.querySelector("span#answer");
+  let res = document.querySelector("span#result");
+  ans.textContent = kaisu + '回目の予想：'+yoso;
+  if (kaisu >= 5) {
+      res.textContent ='答えは'+kotae+'でした.すでにゲームは終わっています';
+  } else if(yoso === kotae) {
+      res.textContent ='正解です.おめでとう！';
+      kaisu = 4;
+  } else if(kaisu === 4) {
+      res.textContent ='まちがい．残念でした答えは '+kotae+' です．';
+  } else if(kotae > yoso) {
+      res.textContent ='まちがい．答えはもっと大きいですよ';
+  } else if(kotae < yoso) {
+      res.textContent ='まちがい．答えはもっと小さいですよ';
+  }
 }
